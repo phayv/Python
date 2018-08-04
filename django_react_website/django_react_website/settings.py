2b33f6249a08dd34d632ccdd939dee41dab13a50
phayrus <vongphay@gmail.com>
2018-08-03 23:47:45 -0700

    initialized with products

--- django_react_website/django_react_website/settings.py
+++ django_react_website/django_react_website/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products.apps.ProductsConfig',
+    'frontend.apps.FrontendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
